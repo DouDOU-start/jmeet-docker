7e67a8fce6f0390ea7d236d6cf4eea42995fd654
“huangenjun” <huangenjun@genew.com>
2023-01-30 19:10:43 +0800

    编译过程支持单个工程编译

--- build.sh
+++ build.sh
@@ -14,6 +14,8 @@ DIR_OF_JICOFO=../jicofo
 DIR_OF_VIDEOBRIDGE=../jitsi-videobridge
 
 if [[ ${COMMAND} == "build" ]]; then
+
+    # 编译完整工程
     if [[ $BUILD_TARGET ==  "all" ]]; then
     
         rm -rf build
@@ -48,16 +50,61 @@ if [[ ${COMMAND} == "build" ]]; then
 
         pushd $DIR_OF_VIDEOBRIDGE > /dev/null
             echo ""
+            echo "start to compile videobridge.."
+            echo ""
+            cd resources && bash build_deb_package.sh
+        popd
+    fi
+
+    # 仅编译meet工程
+    if [[ $BUILD_TARGET ==  "meet" ]]; then
+        pushd $DIR_OF_LIB > /dev/null
+            echo ""
+            echo "start to compile lib-jmeet.."
+            echo ""
+            rm -rf dist
+            npm install
+            npm run build:webpack && npm run build:tsc
+        popd
+
+        pushd $DIR_OF_MEET > /dev/null
+            echo ""
+            echo "start to compile jmeet.."
+            echo ""
+            npm install
+            npm install lib-jitsi-meet --force && make deploy-lib-jitsi-meet
+            rm -rf libs
+            make
+            dpkg-buildpackage -A -rfakeroot -us -uc -tc
+        popd
+    fi
+
+    # 仅编译jicofo工程
+    if [[ $BUILD_TARGET ==  "jicofo" ]]; then
+        pushd $DIR_OF_JICOFO > /dev/null
+            echo ""
             echo "start to compile jicofo.."
             echo ""
             cd resources && bash build_deb_package.sh
         popd
     fi
 
-    mv ../*.deb build/
+    # 仅编译videobridge工程
+    if [[ $BUILD_TARGET ==  "videobridge" ]]; then
+        pushd $DIR_OF_VIDEOBRIDGE > /dev/null
+            echo ""
+            echo "start to compile videobridge.."
+            echo ""
+            cd resources && bash build_deb_package.sh
+        popd
+    fi
+
+    # -f 如果已存在deb包，则强制覆盖
+    mv -f ../*.deb build/
     rm -rf ../*.changes
     rm -rf ../*.buildinfo
 
+    # 开始构建镜像
     docker build -t doudou/${IMAGE_NAME}:${VERSION} .
 
 fi
